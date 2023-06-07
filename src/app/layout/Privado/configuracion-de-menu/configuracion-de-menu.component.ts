0b88fd9abaa171e4afbd9a7bdfc5e97e52846a05
Daniel Carbajo Macedo <16dacm@gmail.com>
2023-06-07 16:17:42 -0500

    Add files via upload

---
+++ src/app/layout/Privado/configuracion-de-menu/configuracion-de-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuracion-de-menu',
+  templateUrl: './configuracion-de-menu.component.html',
+  styleUrls: ['./configuracion-de-menu.component.css']
+})
+export class ConfiguracionDeMenuComponent {
+
+}
---
+++ src/app/layout/Privado/historial-de-venta/historial-de-venta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-de-venta',
+  templateUrl: './historial-de-venta.component.html',
+  styleUrls: ['./historial-de-venta.component.css']
+})
+export class HistorialDeVentaComponent {
+
+}
